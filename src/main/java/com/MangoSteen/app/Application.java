ec682fae430cc6565173895ebf5f8e92b4b1b722
Xitadi-Manfu <1791037454@qq.com>
2024-01-14 15:48:04 +0800

    feat(docs): implement auto API document generation feature.

--- src/main/java/com/MangoSteen/app/Application.java
+++ src/main/java/com/MangoSteen/app/Application.java
@@ -1,9 +1,12 @@
 package com.MangoSteen.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Mangosteen Service", version = "v1", description = "Backend APIS for MangoSteen App"))
 //@MapperScan("mongoose.mapper")
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/MangoSteen/app/controller/UserController.java
+++ src/main/java/com/MangoSteen/app/controller/UserController.java
@@ -4,6 +4,9 @@ package com.MangoSteen.app.controller;
 import com.MangoSteen.app.mapper.UserInfoMapper;
 import com.MangoSteen.app.model.UserInfo;
 import com.MangoSteen.app.service.UserInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.val;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@Tag(name = "User APIS", description = "Related APIS for user management")
 public class UserController {
     // 构造器注入
 
@@ -26,6 +29,11 @@ public class UserController {
 
 
     @GetMapping("v1.0/users/{id}")
+    @Operation(summary = "Get user information", description = "Return the specific user information",
+    responses = {
+            @ApiResponse(responseCode = "200", description = "user information found"),
+            @ApiResponse(responseCode = "404", description = "user information not found")
+    })
     UserInfo getUserInfoById(@PathVariable("id") Long id){
 
         val userInfo = com.MangoSteen.app.model.dao.UserInfo.builder().username("xxx").build();
