247d7163ecb0dd8dbbaabf349eecbecf745af06f
Xitadi-Manfu <1791037454@qq.com>
2024-05-02 00:15:21 +0800

    feat(spring-boot):init springboot service.

---
+++ src/main/java/mangosteen/controller/HelloController.java
@@ -0,0 +1,29 @@
+package mangosteen.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Hello Controller
+ */
+@RestController
+public class HelloController {
+
+    /**
+     * say hello api
+     * @param name name
+     * @param id specific id
+     * @return say hello
+     */
+//    @RequestMapping(path = "v1.0/hello/{name}/{id}", method = RequestMethod.GET)
+    @GetMapping(path = "v1.0/hello/{name}/{id}")
+    public String sayHello(@PathVariable("name") String name, @PathVariable("id") int id){
+        return String.format("Hello, %s 今年 %d 岁" , name,id);
+    }
+
+    @GetMapping(path = "v2.0/greeting")
+    public String myStatus(@RequestParam("name") String name,
+
+                            @RequestParam("status") String status){
+        return String.format("%s 今天的状态时 %s", name, status);
+    }
+}
