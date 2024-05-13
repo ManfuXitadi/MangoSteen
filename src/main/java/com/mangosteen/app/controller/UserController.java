4a65b37869bf35c50c61126a8d34a01d6e0db8b8
Xitadi-Manfu <1791037454@qq.com>
2024-05-13 08:11:04 +0800

    feat(docs):implement auto API document generation feature.

--- src/main/java/com/mangosteen/app/controller/UserController.java
+++ src/main/java/com/mangosteen/app/controller/UserController.java
@@ -3,10 +3,15 @@ package com.mangosteen.app.controller;
 import com.mangosteen.app.mapper.UserInfoMapper;
 import com.mangosteen.app.model.UserInfo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name = "User APIS", description = "Related APIs for user mangosteeen")
 public class UserController {
 
 
@@ -20,7 +25,14 @@ public class UserController {
     }
 
     @GetMapping("v1.0/users/{id}")
-    UserInfo getUserInfoById(@PathVariable("id")Long id){
+    @Operation(summary = "Get user information", description = "return specific user information",
+    responses = {
+      @ApiResponse(responseCode = "200", description = "User information fount"),
+      @ApiResponse(responseCode = "404", description = "User information not found")
+    })
+    UserInfo getUserInfoById(
+            @Parameter(description = "The user id to fetchff")
+            @PathVariable("id")Long id){
         return userInfoMapper.getUserInfoByUserId(id);
     }
 
